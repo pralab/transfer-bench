b597379b9bd2f7211c4ece9b80277bc93c213b67
Fabio Brau <fabio.brau@unica.it>
2025-04-01 17:53:44 +0200

    Add test for model accuracy

--- tests/test_defaults_weights.py
+++ tests/test_defaults_weights.py
@@ -1,17 +1,16 @@
-r"""Test for checking deafult weights of models."""
+r"""Check accuracy for models with  deafult weights."""
 
+import pandas as pd
 import torch
 from transferbench.datasets import get_loader
 from transferbench.models import get_model, list_models
-
-loader = get_loader("ImageNetT", batch_size=256, device="cuda")
 
 
 @torch.no_grad()
 def eval_accuracy(model):
     total = 0
     correct = 0
-    for i, (inputs, labels, _) in enumerate(get_loader("ImageNetT")):
+    for _, (inputs, labels, _) in enumerate(get_loader("ImageNetT")):
         model.eval()
         model.to("cuda")
         inputs, labels = inputs.to("cuda"), labels.to("cuda")
@@ -25,8 +24,17 @@ def eval_accuracy(model):
     return 100 * correct / total
 
 
-print(list_models())
-for model_id in list_models():
-    model = get_model(model_id)
-    acc = eval_accuracy(model)
-    print(f"Model: {model_id}, Accuracy:{acc}")
+if __name__ == "__main__":
+    loader = get_loader("ImageNetT", batch_size=256, device="cuda")
+    # Test the accuracy of each modes
+    acc_dict = {"model_id": [], "accuracy": []}
+    for model_id in list_models():
+        model = get_model(model_id)
+        acc = eval_accuracy(model)
+        acc_dict["model_id"].append(model_id)
+        acc_dict["accuracy"].append(acc)
+
+    # Save the accuracy to a file
+
+    df_accuracy = pd.DataFrame(acc_dict)
+    df_accuracy.to_csv("accuracy.csv", index=False)
