476972bd4f489712dc2746fd676f18ee3eda3139
Fabio Brau <fabio.brau@unica.it>
2025-04-04 15:40:29 +0200

    Code Polishing, no relevant changes

--- wandb_logger.py
+++ wandb_logger.py
@@ -1,22 +1,40 @@
-"""Logger for Weights & Biases"""
+r"""Logger for Weights & Biases."""
 
 import logging
+from typing import Optional
+
 import wandb
 
 
 class WandbHandler(logging.Handler):
-    def __init__(self, project_name: str, entity: str = None, config: dict = None):
+    r"""Custom logging handler for Weights & Biases."""
+
+    def __init__(
+        self,
+        project_name: str,
+        entity: Optional[str] = None,
+        config: Optional[dict] = None,
+    ) -> None:
+        r"""WandbHandler handler for Weights & Biases.
+
+        Parameters
+        ----------
+        - project_name (str): Name of the project.
+        - entity (str, optional): Name of the entity. Defaults to None.
+        - config (dict, optional): Configuration dictionary. Defaults to None.
+        """  # to do Provide a bit of infos on the enitity and config
         super().__init__()
         wandb.init(project=project_name, entity=entity, config=config)
         columns = ["scores", "predictions", "asr", "queries"]
         self.table = wandb.Table(columns=columns)
 
-    def emit(self, record):
+    def emit(self, record: str) -> None:
+        r"""Emit a log record."""
         new_table = wandb.Table(columns=self.table.columns, data=self.table.data)
         new_table.add_data(*record.msg)
         wandb.log({"data": new_table}, commit=False)
         self.table = new_table
 
     @staticmethod
-    def __del__():
+    def __del__() -> None:  # noqa: D105
         wandb.finish()
