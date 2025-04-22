45f643b9e5cf45807ff1581f4924ca41100da08c
Fabio Brau <fabio.brau@unica.it>
2025-04-22 12:25:58 +0200

    Clean __init__ to avoid circular imports

--- transferbench/__init__.py
+++ transferbench/__init__.py
@@ -3,15 +3,4 @@ r"""TransferBench.
 A library for benchmarking transfer-attacks on different scenarios.
 """
 
-from . import attacks_zoo, datasets, models, scenarios, types, utils
 from .attack_evaluation import AttackEval
-
-__all__ = [
-    "AttackEval",
-    "attacks_zoo",
-    "datasets",
-    "models",
-    "scenarios",
-    "types",
-    "utils",
-]
