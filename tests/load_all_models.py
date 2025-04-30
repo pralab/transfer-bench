a0d2372f7b94fd251b5488d487cf5cb83e58283c
Fabio Brau <fabio.brau@unica.it>
2025-04-30 11:13:34 +0200

    Check the accuracy, everything fine

--- tests/load_all_models.py
+++ tests/load_all_models.py
@@ -1,9 +1,35 @@
 r"""Test the pubdef models."""
 
+import torch
+from transferbench.datasets import get_loader
 from transferbench.models import get_model, list_models
 
-available_models = list_models()[::-1]  # reverse the order to load the optional first
-for model_name in available_models:
-    print(f"Loading model: {model_name}: ...", end="\r")
-    _ = get_model(model_name)
-    print(f"Loading model: {model_name}: Done")
+
+# Evaluate the models accuracy on ImageNet
+
+
+@torch.no_grad()
+def acc(model_name: str, batch_size: int = 200) -> float:
+    """Get the accuracy of the model on ImageNet."""
+    model = get_model(model_name)
+    model.eval().to("cuda:2")
+    loader = get_loader("ImageNetT", batch_size=batch_size, device="cuda:2")
+    acc = 0.0
+    for inputs, labels, _ in loader:
+        outputs = model(inputs)
+        acc += (outputs.argmax(1) == labels).float().sum()
+    return acc / 1000
+
+
+def check_models():
+    available_models = list_models()[::-1]
+    for model_name in available_models:
+        print(f"Loading model: {model_name}: ...", end="\r")
+        _ = get_model(model_name)
+        print(f"Loading model: {model_name}: Done. Accuracy: {acc(model_name)}")
+
+    print("All models loaded successfully.")
+
+
+if __name__ == "__main__":
+    check_models()
