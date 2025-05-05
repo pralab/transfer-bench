288d17a51b3d41186e81a786ae7f341980f3abc7
Fabio Brau <fabio.brau@unica.it>
2025-05-05 18:01:59 +0200

    Bugfix

--- tests/test_zeroquery.py
+++ tests/test_zeroquery.py
@@ -23,4 +23,4 @@ def main():
         scenario = load_attack_scenario("debug")[0]
         scenario.dataset = Subset(ImageNetT(), range(10))
         evaluator.set_scenarios(scenario)
-        _ = evaluator.run(batch_size=2, device="cuda:2")
+        _ = evaluator.run(batch_size=2, device="cuda:1")
