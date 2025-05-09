4631483d9834241ee0ec664463181e4638cc3941
Fabio Brau <fabio.brau@unica.it>
2025-05-09 12:49:18 +0200

    Make cifar models optional (no need of huggface

--- transferbench/models/__init__.py
+++ transferbench/models/__init__.py
@@ -1,12 +1,6 @@
 r"""Load model and offer utils for transfer/attack evaluations."""
 
-from . import utils
-from .cifar_models import (
-    get_model as get_cifar_model,
-)
-from .cifar_models import (
-    list_models as list_cifar_models,
-)
+# default models
 from .imagenet_models import (
     get_model as get_imagenet_model,
 )
@@ -14,7 +8,7 @@ from .imagenet_models import (
     list_models as list_imagenet_models,
 )
 
-OPTIONAL_MODELS = [
+OPTIONAL_ROBUST_MODELS = [
     "imagenet_resnet50_pubdef",  # imagenet
     "Xu2024MIMIR_Swin-L",  # imagenet
     "Amini2024MeanSparse_Swin-L",  # imagenet
@@ -26,7 +20,9 @@ OPTIONAL_MODELS = [
 
 def get_model(model_name: str) -> None:
     r"""Load a model from Imagenet or RobustBench."""
-    if model_name in OPTIONAL_MODELS:
+    if model_name in list_imagenet_models():
+        return get_imagenet_model(model_name)
+    if model_name in OPTIONAL_ROBUST_MODELS:
         try:
             from .pubdef_models import (
                 get_model as get_pubdef_model,
@@ -52,13 +48,46 @@ def get_model(model_name: str) -> None:
             )
             raise ImportError(msg) from e
     if "cifar" in model_name:
-        return get_cifar_model(model_name)
-    return get_imagenet_model(model_name)
+        try:
+            from .cifar_models import (
+                get_model as get_cifar_model,
+            )
+
+            return get_cifar_model(model_name)
+        except ImportError as e:
+            msg = (
+                "For cifar models, please install all the dependencies "
+                " with `pip install transferbench[cifar]`."
+            )
+            raise ImportError(msg) from e
+    msg = f"Model {model_name} not found."
+    raise ValueError(msg)
 
 
 def list_models() -> list[str]:
     r"""List all available models."""
-    return list_imagenet_models() + list_cifar_models() + OPTIONAL_MODELS
+    models_list = list_imagenet_models()
+    try:
+        from .pubdef_models import (
+            list_models as list_pubdef_models,
+        )
+        from .robust_models import (
+            list_models as list_robust_models,
+        )
+
+        models_list.extend(list_robust_models() + list_pubdef_models())
+    except ImportError:
+        pass
+    try:
+        from .cifar_models import (
+            list_models as list_cifar_models,
+        )
+
+        models_list.extend(list_cifar_models())
+    except ImportError:
+        pass
+
+    return sorted(models_list)
 
 
-__all__ = ["get_model", "list_models", "utils"]
+__all__ = ["get_model", "list_models"]
